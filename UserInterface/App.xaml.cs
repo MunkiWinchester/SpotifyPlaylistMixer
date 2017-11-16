78a335747f798404298f215abec7e9f0f792b832
Eike Stremlau <eike.stremlau@emp.de>
2017-11-16 14:13:47 +0100

    Made the application single instance only (Added mutex)

--- UserInterface/App.xaml.cs
+++ UserInterface/App.xaml.cs
@@ -1,9 +1,60 @@
-﻿namespace UserInterface
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace UserInterface
 {
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        #region User32.dll import
+        [DllImport("user32", CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindow(string cls, string win);
+        [DllImport("user32")]
+        private static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32")]
+        private static extern bool IsIconic(IntPtr hWnd);
+        [DllImport("user32")]
+        private static extern bool OpenIcon(IntPtr hWnd);
+        #endregion
+
+        private static readonly string Name = Assembly.GetExecutingAssembly().GetName().Name;
+        private static Mutex _appMutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _appMutex = new Mutex(true, Name, out bool isNew);
+            if (!isNew)
+            {
+                ActivateOtherWindow();
+                _appMutex = null;
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _appMutex?.ReleaseMutex();
+            base.OnExit(e);
+        }
+
+        private static void ActivateOtherWindow()
+        {
+            // TODO: Don't like the hard coded title of the start window..
+            var other = FindWindow(null, "Spotify Mixer");
+            if (other != IntPtr.Zero)
+            {
+                SetForegroundWindow(other);
+                if (IsIconic(other))
+                    OpenIcon(other);
+            }
+        }
     }
 }
